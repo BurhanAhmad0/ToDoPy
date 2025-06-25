6b1a6e3143266d4e61300598e490d07a957ca565
Burhan Ahmad <burhan7554@gmail.com>
2025-06-25 15:50:54 +0500

    Refactor code structure for improved readability and maintainability

--- client/src/layouts/AuthLayout.jsx
+++ client/src/layouts/AuthLayout.jsx
@@ -1,29 +1,29 @@
-import React from 'react'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import { Outlet } from "react-router-dom";
 
-import HeroImage from '../assets/icons/heroImage.png'
+import HeroImage from "../assets/icons/heroImage.webp";
 
 const AuthLayout = () => {
-    return (
-        <section className="authLayout bgImage bg-primary-background dark:bg-black min-h-screen flex flex-col lg:flex-row gap-6 p-6 sm:p-10">
-            {/* Left Section - Image */}
-            <div className="leftSection w-full lg:w-1/2 hidden lg:flex justify-center items-center">
-                <img
-                    loading="lazy"
-                    src={HeroImage}
-                    alt="Hero"
-                    className="w-full max-w-md md:max-w-lg lg:max-w-full h-auto object-contain"
-                />
-            </div>
+  return (
+    <section className="authLayout bgImage bg-primary-background dark:bg-black min-h-screen flex flex-col lg:flex-row gap-6 p-6 sm:p-10">
+      {/* Left Section - Image */}
+      <div className="leftSection w-full lg:w-1/2 hidden lg:flex justify-center items-center">
+        <img
+          loading="lazy"
+          src={HeroImage}
+          alt="Hero"
+          className="w-full max-w-md md:max-w-lg lg:max-w-full h-auto object-contain"
+        />
+      </div>
 
-            {/* Right Section - Form/Content */}
-            <div className="rightSection w-full lg:w-1/2 bg-secondary-background rounded-3xl p-6 sm:p-10 md:px-16 lg:px-20 flex items-center justify-center">
-                <div className="w-full">
-                    <Outlet />
-                </div>
-            </div>
-        </section>
-    )
-}
+      {/* Right Section - Form/Content */}
+      <div className="rightSection w-full lg:w-1/2 bg-secondary-background rounded-3xl p-6 sm:p-10 md:px-16 lg:px-20 flex items-center justify-center">
+        <div className="w-full">
+          <Outlet />
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default AuthLayout
+export default AuthLayout;
--- client/src/pages/NotFound.jsx
+++ client/src/pages/NotFound.jsx
@@ -1,20 +1,24 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
-import { useAuth } from '../context/AuthContext.jsx'
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../context/AuthContext.jsx";
 
-import NotFoundImage from '../assets/icons/notFound.jpg'
+import NotFoundImage from "../assets/icons/notFound.webp";
 
 const NotFound = () => {
-
-  const navigate = useNavigate()
+  const navigate = useNavigate();
   const { user, Loading } = useAuth();
 
   return (
-    <section className='not-found w-full h-screen flex flex-col items-center justify-center'>
-      <img className='w-96' loading='lazy' src={NotFoundImage} alt="" />
-      <button onClick={()=>navigate(`/${user?.firstName.toLowerCase()}`)} className='bg-[#59e4a8] w-52 py-4 rounded-lg mt-5 hover:bg-[#59e4a8]/80 cursor-pointer transition-all duration-300'>Go Back Home</button>
+    <section className="not-found w-full h-screen flex flex-col items-center justify-center">
+      <img className="w-96" loading="lazy" src={NotFoundImage} alt="" />
+      <button
+        onClick={() => navigate(`/${user?.firstName.toLowerCase()}`)}
+        className="bg-[#59e4a8] w-52 py-4 rounded-lg mt-5 hover:bg-[#59e4a8]/80 cursor-pointer transition-all duration-300"
+      >
+        Go Back Home
+      </button>
     </section>
-  )
-}
+  );
+};
 
-export default NotFound
+export default NotFound;
